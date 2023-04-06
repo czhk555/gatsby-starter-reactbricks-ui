45770c0367790d629a9303368b2e433898284587
FrancescoMottaDev <francesco.motta@reactbricks.com>
2023-04-06 17:23:23 +0200

    import nunito sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
         newsLetter:
           '0 1px 3px 0 rgba(0, 0, 0, 0.1) , 0 5px 15px 0 rgba(0, 0, 0, 0.05)',
       },
+      fontFamily: {
+        sans: ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [require('@tailwindcss/forms')],
