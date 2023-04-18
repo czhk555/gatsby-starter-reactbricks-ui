46a16a79444fa382fa643158ac62f8fac3fdad35
FrancescoMottaDev <francesco.motta@reactbricks.com>
2023-04-18 09:55:49 +0200

    Layout : darkMode bg color fixed

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -13,7 +13,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         isDarkColorMode ? 'dark' : 'light'
       } flex flex-col h-screen justify-between font-content antialiased`}
     >
-      <main className="mb-auto">{children}</main>
+      <main className="mb-auto dark:bg-gray-900">{children}</main>
     </div>
   )
 }
