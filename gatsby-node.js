12861e1c6c257a62e6c477a907ae9469f39a89c3
paolozanotti-f2 <paolo.zanotti@reactbricks.com>
2023-12-11 11:35:35 +0100

    ⬆️ Update RB to 4.1.0 and add .env environment var

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,6 +10,7 @@ const fetchTags = require('react-bricks/frontend').fetchTags
 exports.createPages = async ({ actions: { createPage } }) => {
   const appId = process.env.GATSBY_APP_ID
   const apiKey = process.env.API_KEY
+  const publicEnvironment = process.env.GATSBY_PUBLIC_ENVIRONMENT
 
   let errorHeader = false
   let errorFooter = false
